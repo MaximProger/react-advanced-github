8f5548ee07a139c484e976c000efed24f0d951bf
MaximProger <maksimkoshman090@gmail.com>
2023-03-13 12:10:50 +0300

    project is ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import FavouritesPage from "./pages/FavouritesPage";
+import Navigation from "./components/Navigation";
 
 function App() {
-  return <h1>Hello</h1>;
+  return (
+    <>
+      <Navigation />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/favourites" element={<FavouritesPage />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
